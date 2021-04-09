b6f8625c474a85335e9584e858eac17220513865
YunYouJun <me@yunyoujun.cn>
2021-04-09 13:27:57 +0800

    :lipstick: optimize style font(14px) and contact layout

--- src/components/resume/Header.vue
+++ src/components/resume/Header.vue
@@ -1,22 +1,18 @@
 <template>
   <div>
-    <h1 class="name text-center text-4xl">{{ resume.name }}</h1>
+    <h1 class="font-normal text-center text-4xl">{{ resume.name }}</h1>
     <small class="flex justify-center">
       <span class="mx-1">{{ resume.info.sex }}</span>
       <span class="mx-1">{{ resume.info.birth }}</span>
       <span class="mx-1">{{ resume.info.location }}</span>
     </small>
-    <ul class="my-4 flex justify-center list-none">
+    <ul class="my-4 block text-center list-none" style="padding-left: 0">
       <li
-        class="mx-2 inline"
+        class="mx-3 inline-block"
         v-for="contact in resume.contact"
         :key="contact.label"
       >
-        <a
-          class="nav-link text-decoration-none"
-          :href="contact.href"
-          target="_blank"
-        >
+        <a class="text-decoration-none" :href="contact.href" target="_blank">
           <iconify-icon :icon="contact.icon" />
           {{ contact.label }}
         </a>
@@ -25,20 +21,15 @@
   </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ResumeInfo } from '../../types'
 import IconifyIcon from '../IconifyIcon.vue'
 
 export default defineComponent({
   components: { IconifyIcon },
   props: {
-    resume: Object,
+    resume: Object as PropType<ResumeInfo>,
   },
 })
 </script>
-
-<style lang="scss">
-.name {
-  font-weight: 400;
-}
-</style>
