5f60f7c967ceda42c6b49120a3010c1ea8c850f0
Fa鸽 <43724908+Akarinnnnn@users.noreply.github.com>
2023-03-16 10:22:36 +0800

    添加头像[2]
    更新Header组件

--- src/components/resume/Header.vue
+++ src/components/resume/Header.vue
@@ -13,9 +13,12 @@ const name = computed(() => {
 
 <template>
   <div v-if="resume">
-    <h1 class="font-normal text-center text-4xl">
-      {{ name || 'Your Name' }}
-    </h1>
+    <div class="flex justify-between">
+      <span class="font-normal text-center text-4xl">
+        {{ name || 'Your Name' }}
+      </span>
+      <img v-if="resume.basics.avatar" :src="resume.basics.avatar" style="max-height: 3cm; max-width: 3cm;">
+    </div>
     <small v-if="resume.basics" class="flex justify-center">
       <span class="mx-1">{{ resume.basics.sex }}</span>
       <span class="mx-1">{{ resume.basics.birth }}</span>
