2d6b75f172bc5968e268a950abddd22ca396c2f2
YunYouJun <me@yunyoujun.cn>
2023-10-11 09:43:50 +0800

    chore: upgrade deps

--- src/components/resume/Education.vue
+++ src/components/resume/Education.vue
@@ -10,30 +10,28 @@ withDefaults(defineProps<{ education: Education | null }>(), {
 <template>
   <div v-if="education">
     <base-title :icon="education.icon" :title="education.title" />
-    <div>
-      <div
-        v-for="(edu, index) in education.histories"
-        :key="index"
-        class="mb-2"
-      >
-        <div>
-          <img v-if="edu.logo" class="school-logo" :src="edu.logo">
-          <span v-else-if="edu.icon" class="mr-1">
-            <IconifyIcon :icon="edu.icon" />
-          </span>
-          <span>
-            <strong>{{ edu.school }}</strong>
-            <IconifyIcon class="position-icon" icon="ri:map-pin-line" />
-            {{ edu.city }}
-          </span>
-          <span class="float-right">
-            {{ `${edu.start} ~ ${edu.end}` }}
-          </span>
-        </div>
-        <div>
-          <span>{{ edu.studyType }}</span>， <span>{{ edu.area }}</span>
-          <span v-if="edu.grade">，{{ edu.grade }}</span>
-        </div>
+    <div
+      v-for="(edu, index) in education.histories"
+      :key="index"
+      class="ml-2 mb-2"
+    >
+      <div>
+        <img v-if="edu.logo" class="school-logo" :src="edu.logo">
+        <span v-else-if="edu.icon" class="mr-1">
+          <IconifyIcon :icon="edu.icon" />
+        </span>
+        <span>
+          <strong>{{ edu.school }}</strong>
+          <IconifyIcon class="position-icon" icon="ri:map-pin-line" />
+          {{ edu.city }}
+        </span>
+        <span class="float-right">
+          {{ `${edu.start} ~ ${edu.end}` }}
+        </span>
+      </div>
+      <div mt-0.5 text-sm>
+        <span>{{ edu.studyType }}</span>， <span>{{ edu.area }}</span>
+        <span v-if="edu.grade">，{{ edu.grade }}</span>
       </div>
     </div>
   </div>
